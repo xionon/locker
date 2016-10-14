f02ef84fa92f71b70654fdbb4dc5eb153557f7fc
Alec Hipshear <alechipshear@gmail.com>
2016-10-13 21:11:16 -0400

    Helper button to create new orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,4 +8,17 @@ class OrdersController < ApplicationController
       .includes(:products, purchases: [:shipments])
       .find(params[:id])
   end
+
+  def create
+    @order = Order.create.tap do |order|
+      (rand(10)+3).times do
+        Purchase.create(
+          order: order,
+          product: Product.all.sample
+        )
+      end
+    end
+
+    redirect_to @order
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :orders, only: %i[index show]
+  resources :orders, only: %i[index show create]
 
   root to: 'orders#index'
 end
